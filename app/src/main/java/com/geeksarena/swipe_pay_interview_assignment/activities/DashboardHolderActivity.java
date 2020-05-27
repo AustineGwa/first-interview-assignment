a77211387f8e16ce57ce1d075c3e5d20d43d8e81
AustineGwa <austinegwa64@gmail.com>
2020-05-26 22:52:23 -0400

    created fragments and activities

---
+++ app/src/main/java/com/geeksarena/swipe_pay_interview_assignment/activities/DashboardHolderActivity.java
@@ -0,0 +1,16 @@
+package com.geeksarena.swipe_pay_interview_assignment.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.geeksarena.swipe_pay_interview_assignment.R;
+
+public class DashboardHolderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+    }
+}
