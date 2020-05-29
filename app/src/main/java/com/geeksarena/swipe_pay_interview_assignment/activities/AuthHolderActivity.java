f0203b528f4a5c43e354d685b493c9b9f9d66c92
AustineGwa <austinegwa64@gmail.com>
2020-05-29 04:09:13 -0400

    fix gragment loading  glitches

--- app/src/main/java/com/geeksarena/swipe_pay_interview_assignment/activities/AuthHolderActivity.java
+++ app/src/main/java/com/geeksarena/swipe_pay_interview_assignment/activities/AuthHolderActivity.java
@@ -1,10 +1,13 @@
 package com.geeksarena.swipe_pay_interview_assignment.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.geeksarena.swipe_pay_interview_assignment.R;
+import com.geeksarena.swipe_pay_interview_assignment.fragments.LoginFragment;
 
 public class AuthHolderActivity extends AppCompatActivity {
 
@@ -12,5 +15,11 @@ public class AuthHolderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.auth_layout_holder, new LoginFragment());
+        transaction.commit();
+
     }
 }
