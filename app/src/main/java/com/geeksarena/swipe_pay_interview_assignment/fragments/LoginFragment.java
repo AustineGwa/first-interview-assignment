e3ebc83ed31f7a8f528d2f9d2c9936c5491acb28
AustineGwa <austinegwa64@gmail.com>
2020-05-29 03:59:52 -0400

    finish login page

--- app/src/main/java/com/geeksarena/swipe_pay_interview_assignment/fragments/LoginFragment.java
+++ app/src/main/java/com/geeksarena/swipe_pay_interview_assignment/fragments/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.geeksarena.swipe_pay_interview_assignment.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.geeksarena.swipe_pay_interview_assignment.R;
+import com.geeksarena.swipe_pay_interview_assignment.activities.DashboardHolderActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +26,14 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        view.findViewById(R.id.login_bt).setOnClickListener(v -> showHome());
+        
+        return view;
+    }
+
+    private void showHome() {
+        startActivity(new Intent(getActivity(), DashboardHolderActivity.class));
+        getActivity().finish();
     }
 }
