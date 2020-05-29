3056908a42b79aafafde78e4420fdf7d9f685d07
AustineGwa <austinegwa64@gmail.com>
2020-05-29 04:51:02 -0400

    implement dashboard template

--- app/src/main/java/com/geeksarena/swipe_pay_interview_assignment/activities/DashboardHolderActivity.java
+++ app/src/main/java/com/geeksarena/swipe_pay_interview_assignment/activities/DashboardHolderActivity.java
@@ -1,10 +1,15 @@
 package com.geeksarena.swipe_pay_interview_assignment.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.geeksarena.swipe_pay_interview_assignment.R;
+import com.geeksarena.swipe_pay_interview_assignment.fragments.LoginFragment;
+import com.geeksarena.swipe_pay_interview_assignment.fragments.UserDashboardFragment;
 
 public class DashboardHolderActivity extends AppCompatActivity {
 
@@ -12,5 +17,15 @@ public class DashboardHolderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
+
+        showFragment(new UserDashboardFragment());
+    }
+
+    private void showFragment(Fragment fragment) {
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.dashboard_frame, fragment);
+        transaction.commit();
     }
 }
